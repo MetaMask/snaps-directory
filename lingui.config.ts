b59622cceea7f49b58e21fa5af97e29b1c9aed27
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-09-04 10:54:25 +0200

    Use po files

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,11 +1,9 @@
 import type { LinguiConfig } from '@lingui/conf';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { formatter } from '@lingui/format-json';
 
 const config: LinguiConfig = {
   compileNamespace: 'cjs',
   locales: ['en'],
-  format: formatter({ style: 'lingui' }),
+  format: 'po',
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
