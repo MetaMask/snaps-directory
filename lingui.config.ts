3c1ecc6b6a40f20aeae1c1950ee8e639be5d217e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-09-06 09:54:55 +0200

    Remove line numbers from translation file (#35)

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,9 +1,10 @@
 import type { LinguiConfig } from '@lingui/conf';
+import { formatter } from '@lingui/format-po';
 
 const config: LinguiConfig = {
   compileNamespace: 'es',
   locales: ['en'],
-  format: 'po',
+  format: formatter({ lineNumbers: false }),
   catalogs: [
     {
       path: '<rootDir>/src/locales/{locale}/messages',
