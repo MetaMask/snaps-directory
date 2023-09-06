b706fb995b16ca1a70e40a94fa142c7659a463af
Frederik Bolding <frederik.bolding@gmail.com>
2023-09-06 12:44:51 +0200

    Add more caching to NPM fetching (#39)

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-nodejs-modules */
 import { detectSnapLocation } from '@metamask/snaps-controllers/dist/snaps/location';
+import deepEqual from 'fast-deep-equal';
+import { rm } from 'fs/promises';
 import type { GatsbyNode } from 'gatsby';
 import type { RequestInfo, RequestInit } from 'node-fetch';
 import fetch from 'node-fetch';
 import { fetchBuilder, FileSystemCache } from 'node-fetch-cache';
-// eslint-disable-next-line import/no-nodejs-modules
+import path from 'path';
 import semver from 'semver/preload';
 
 export const sourceNodes: GatsbyNode[`sourceNodes`] = async ({
@@ -13,25 +16,40 @@ export const sourceNodes: GatsbyNode[`sourceNodes`] = async ({
 }) => {
   const { createNode } = actions;
 
-  const registry = await fetch(
-    'https://acl.execution.consensys.io/latest/registry.json',
-    {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-      },
-    },
-  ).then(async (response) => response.json());
+  const registryUrl = 'https://acl.execution.consensys.io/latest/registry.json';
+
+  const headers = {
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+  };
+
+  const cachedTarballFetch = fetchBuilder.withCache(
+    new FileSystemCache({ cacheDirectory: '.cache/npm/tarballs' }),
+  );
+
+  const fetchCachePath = '.cache/npm/fetch';
 
   const cachedFetch = fetchBuilder.withCache(
-    new FileSystemCache({ cacheDirectory: '.cache/npm' }),
+    new FileSystemCache({ cacheDirectory: fetchCachePath }),
   );
+
+  const registry = await fetch(registryUrl, { headers }).then(
+    async (response) => response.json(),
+  );
+
+  const cachedRegistry = await cachedFetch(registryUrl, { headers }).then(
+    async (response) => response.json(),
+  );
+
+  if (!deepEqual(cachedRegistry, registry)) {
+    await rm(path.resolve(fetchCachePath), { recursive: true });
+  }
 
   const customFetch = (url: RequestInfo, options: RequestInit | undefined) => {
     if (url.toString().endsWith('.tgz')) {
-      return cachedFetch(url, options);
+      return cachedTarballFetch(url, options);
     }
-    return fetch(url, options);
+    return cachedFetch(url, options);
   };
 
   // TODO: Fix types.
