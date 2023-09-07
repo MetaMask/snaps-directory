cf8c5bf426625aeb243033f503d8c7eaa1bf809c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-09-07 14:20:58 +0200

    Fix home page hydration error (#46)

---
+++ src/components/SnapsGrid.tsx
@@ -0,0 +1,21 @@
+import { SimpleGrid } from '@chakra-ui/react';
+import type { FunctionComponent } from 'react';
+
+import type { Snap } from './providers';
+import { SnapCard } from './SnapCard';
+
+export type SnapsGridProps = {
+  snaps: Snap[];
+};
+
+export const SnapsGrid: FunctionComponent<SnapsGridProps> = ({ snaps }) => (
+  <SimpleGrid columns={[1, null, 2, 3]} spacing={4}>
+    {snaps
+      .filter((snap) => !snap.snapId.endsWith('example-snap'))
+      .map((snap) => (
+        <SnapCard key={snap.id} {...snap} />
+      ))}
+  </SimpleGrid>
+);
+
+export default SnapsGrid;
