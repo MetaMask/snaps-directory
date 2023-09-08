44e1c650b3d28db679b40ed458802fd263c14155
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-09-08 13:00:56 +0200

    Add 404 page (#54)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,95 @@
-import { Text, Box, Container, Center, Heading } from '@chakra-ui/react';
+import { Container, Heading, Button } from '@chakra-ui/react';
 import { Trans } from '@lingui/macro';
+import { graphql, Link } from 'gatsby';
+import type { FunctionComponent } from 'react';
+
+import banner from '../assets/images/seo/home.png';
+import { Fox } from '../components';
+import type { Fields } from '../utils';
 
 const NotFoundPage = () => {
   return (
-    <Box p="8" textAlign="center">
-      <Container>
-        <Center>
-          <Heading m="2" as="h1" size="2xl">
-            404
-          </Heading>
-        </Center>
-        <Text mt="4" opacity="0.75">
-          <Trans>These are not the droids you are looking for.</Trans>
-        </Text>
-      </Container>
-    </Box>
+    <Container
+      display="flex"
+      flexDirection="column"
+      marginY="12"
+      alignItems="center"
+      textAlign="center"
+      maxWidth="container.md"
+    >
+      <Fox />
+      <Heading
+        as="h1"
+        fontFamily="custom"
+        fontWeight="500"
+        fontSize={['2xl', '4xl', '5xl']}
+        marginY="6"
+        lineHeight="116%"
+      >
+        <Trans>The page you&apos;re looking for can&apos;t be found.</Trans>
+      </Heading>
+      <Link to="/">
+        <Button variant="primary">
+          <Trans>Go home</Trans>
+        </Button>
+      </Link>
+    </Container>
   );
 };
+
+type HeadProps = {
+  data: {
+    site: {
+      siteMetadata: Fields<
+        Queries.SiteSiteMetadata,
+        'title' | 'description' | 'author' | 'siteUrl'
+      >;
+    };
+  };
+};
+
+export const Head: FunctionComponent<HeadProps> = ({ data }) => {
+  const title = `Page not found - ${data.site.siteMetadata.title}`;
+  const image = `${data.site.siteMetadata.siteUrl}${banner}`;
+
+  return (
+    <>
+      <html lang="en" />
+      <title>{title}</title>
+      <meta name="description" content={data.site.siteMetadata.description} />
+      <meta property="og:title" content="Page not found" />
+      <meta property="og:site_name" content={data.site.siteMetadata.title} />
+      <meta
+        property="og:description"
+        content={data.site.siteMetadata.description}
+      />
+      <meta property="og:type" content="website" />
+      <meta name="og:image" content={image} />
+      <meta name="og:image:width" content="1200" />
+      <meta name="og:image:height" content="630" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={data.site.siteMetadata.author} />
+      <meta name="twitter:title" content={title} />
+      <meta
+        name="twitter:description"
+        content={data.site.siteMetadata.description}
+      />
+      <meta name="twitter:image" content={image} />
+    </>
+  );
+};
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        siteUrl
+      }
+    }
+  }
+`;
 
 export default NotFoundPage;
