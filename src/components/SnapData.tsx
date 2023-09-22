47ee7ba70e85e50127a48aa60b7dcc1df25c950e
David Drazic <david@timechaser.org>
2023-09-22 16:05:15 +0200

    Move description in the middle of metadata on mobile screens (#105)
    Fixes: https://github.com/MetaMask/snaps-directory/issues/104
    
    This PR will add some adjustments to handle displaying description of a
    Snap in the middle of its metadata. It also changes the order of the
    metadata fields as requested, on mobile screen resolutions. Large
    (desktop) and medium size resolutions are left untouched.
    
    ## After changes
    ![Screenshot 2023-09-13 at 14 22
    10](https://github.com/MetaMask/snaps-directory/assets/13301024/72b01bc5-ac95-4c77-818f-a7a7cd8a211e)
    ![localhost_8000_snap_npm_btcsnap_(Pixel 5)
    (1)](https://github.com/MetaMask/snaps-directory/assets/13301024/e3984a68-91dc-4a47-935e-ec6bff550932)
    
    ## Before changes
    ![Screenshot 2023-09-13 at 14 23
    09](https://github.com/MetaMask/snaps-directory/assets/13301024/bcf87957-601c-4a27-bdad-0c103dea5183)
    ![snaps metamask io_snap_npm_btcsnap_(Pixel
    5)](https://github.com/MetaMask/snaps-directory/assets/13301024/b6e879a2-d283-4ad2-991b-94d2ef0856d2)
    
    ---------
    
    Co-authored-by: eriknson <eriks@mail.se>
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>

--- src/components/SnapData.tsx
+++ src/components/SnapData.tsx
@@ -1,16 +1,20 @@
+import type { ResponsiveValue } from '@chakra-ui/react';
 import { Flex, Text } from '@chakra-ui/react';
 import type { FunctionComponent, ReactNode } from 'react';
 
 export type SnapDataProps = {
   label: string;
   value: ReactNode;
+  order: ResponsiveValue<number>;
 };
 
 export const SnapData: FunctionComponent<SnapDataProps> = ({
   label,
   value,
+  order,
 }) => (
   <Flex
+    order={order}
     flexDirection="column"
     minWidth={{ base: 'auto', md: '220px', lg: 'auto' }}
   >
--- src/pages/snap/{Snap.location}/{Snap.slug}.tsx
+++ src/pages/snap/{Snap.location}/{Snap.slug}.tsx
@@ -70,7 +70,7 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
             flexDirection={{ base: 'column', md: 'row' }}
             marginTop={{ base: 4, md: 0 }}
             width={{ base: '100%', md: 'auto' }}
-            gap={{ base: 2, md: 4 }}
+            gap={{ base: 4, md: 4 }}
           >
             {data.snap.website && (
               <Link
@@ -114,6 +114,7 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
         >
           {data.snap.category && (
             <SnapData
+              order={{ base: 1, md: 1 }}
               label={t`Category`}
               value={
                 <SnapCategory
@@ -122,9 +123,14 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
               }
             />
           )}
-          <SnapData label={t`Version`} value={latestVersion} />
+          <SnapData
+            order={{ base: 8, md: 2 }}
+            label={t`Version`}
+            value={latestVersion}
+          />
           {data.snap.author && (
             <SnapData
+              order={{ base: 2, md: 3 }}
               label={t`Developer`}
               value={
                 <ExternalLink href={data.snap.author.website}>
@@ -139,18 +145,63 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
             // system for every screen.
           }
           <Box
+            order={{ base: 1, md: 4 }}
             display={{ base: 'none', md: 'flex', lg: 'none' }}
             flexBasis="100%"
             height={0}
           />
+          {
+            // On mobile screens description is displayed in the middle
+            // of the Snap's metadata
+          }
+          <Divider
+            my="2"
+            display={{ base: 'flex', md: 'none' }}
+            order={{ base: 4, md: 5 }}
+          />
+          <Text
+            display={{ base: 'block', md: 'none' }}
+            order={{ base: 5, md: 6 }}
+            color="gray.muted"
+            textTransform="uppercase"
+            fontWeight="medium"
+            fontSize="sm"
+          >
+            <Trans>
+              Description by{' '}
+              <Text
+                as="span"
+                color="black"
+                textTransform="uppercase"
+                fontWeight="medium"
+                fontSize="sm"
+              >
+                {name}
+              </Text>
+            </Trans>
+          </Text>
+          <SnapDescription
+            order={{ base: 6, md: 7 }}
+            description={description}
+            mt="1"
+            whiteSpace="pre-wrap"
+            display={{ base: 'flex', md: 'none' }}
+          />
+          <Divider
+            my="2"
+            display={{ base: 'flex', md: 'none' }}
+            order={{ base: 7, md: 8 }}
+          />
           {data.snap.sourceCode && (
             <SnapData
+              order={{ base: 9, md: 9 }}
               label={t`Source Code`}
               value={<SnapSourceCode url={data.snap.sourceCode} />}
             />
           )}
           {data.snap.audits && (
             <SnapData
+              order={{ base: 10, md: 10 }}
               label={t`Audit`}
               value={
                 <SnapAudits
@@ -168,6 +219,7 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
             data.snap.support?.faq ||
             data.snap.support?.knowledgeBase) && (
             <SnapData
+              order={{ base: 3, md: 11 }}
               label={t`Support`}
               value={
                 <>
@@ -191,8 +243,9 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
             />
           )}
         </Flex>
-        <Divider my="6" />
+        <Divider my="6" display={{ base: 'none', md: 'flex' }} />
         <Text
+          display={{ base: 'none', md: 'block' }}
           color="gray.muted"
           textTransform="uppercase"
           fontWeight="medium"
@@ -212,6 +265,7 @@ const SnapPage: FunctionComponent<SnapPageProps> = ({ data }) => {
           </Trans>
         </Text>
         <SnapDescription
+          display={{ base: 'none', md: 'flex' }}
           description={description}
           mt="1"
           whiteSpace="pre-wrap"
