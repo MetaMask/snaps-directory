48e786792ea7c7816bf84d81dafa825ba00e8186
Mrtenz <Mrtenz@users.noreply.github.com>
2023-09-26 08:49:02 +0000

    deploy: d3e38bf6ed3105f7e658d2cf78293f17272844d2

--- 485-bdcdc79921e7e2fb8577.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_metamask_snaps_directory=self.webpackChunk_metamask_snaps_directory||[]).push([[485],{7485:function(s,n,a){a.r(n),a.d(n,{SnapsGrid:function(){return c}});var e=a(7746),r=a(2688),t=a(5893);const c=s=>{let{snaps:n}=s;return(0,t.jsx)(e.M,{columns:[1,null,2,3],spacing:4,children:n.map(((s,n)=>(0,t.jsx)(r.z,{...s},s.id+"-"+n)))})};n.default=c}}]);
-//# sourceMappingURL=485-bdcdc79921e7e2fb8577.js.map
