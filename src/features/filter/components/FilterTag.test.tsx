a89e2ed0f8c0e669921d6421d19442d78041af3b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-10-01 15:11:21 +0200

    Test src/features/filter/components/FilterTag.tsx

---
+++ src/features/filter/components/FilterTag.test.tsx
@@ -0,0 +1,36 @@
+import { act } from '@testing-library/react';
+
+import { FilterTag } from './FilterTag';
+import { RegistrySnapCategory } from '../../../constants';
+import { createStore } from '../../../store';
+import { render } from '../../../utils/test-utils';
+import { getCategory } from '../store';
+
+describe('FilterTag', () => {
+  it('renders', () => {
+    const { queryByText } = render(
+      <FilterTag category={RegistrySnapCategory.TransactionInsights} />,
+    );
+
+    expect(queryByText('Transaction Insights')).toBeInTheDocument();
+  });
+
+  it('toggles the category when clicked', () => {
+    const store = createStore();
+    const { getByLabelText } = render(
+      <FilterTag category={RegistrySnapCategory.TransactionInsights} />,
+      store,
+    );
+
+    expect(
+      getCategory(RegistrySnapCategory.TransactionInsights)(store.getState()),
+    ).toBe(true);
+
+    const button = getByLabelText('Close').parentElement;
+    act(() => button?.click());
+
+    expect(
+      getCategory(RegistrySnapCategory.TransactionInsights)(store.getState()),
+    ).toBe(false);
+  });
+});
