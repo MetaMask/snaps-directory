7b45b7d1e949951fafbb9cec1497b5cba85d591b
Frederik Bolding <frederik.bolding@gmail.com>
2024-05-10 10:56:05 +0200

    Fix missing skeleton color

--- src/theme/components/skeleton.ts
+++ src/theme/components/skeleton.ts
@@ -3,7 +3,7 @@ import { cssVar, defineStyle, defineStyleConfig } from '@chakra-ui/react';
 export const Skeleton = defineStyleConfig({
   baseStyle: defineStyle({
     [cssVar('skeleton-start-color').variable]: 'colors.background.alternative',
-    [cssVar('skeleton-end-color').variable]: 'colors.border',
+    [cssVar('skeleton-end-color').variable]: 'colors.border.muted',
     borderRadius: 'lg',
   }),
 });
