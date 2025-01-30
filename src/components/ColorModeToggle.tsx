f75fc74343e36600e97c8000160e2fe9d0ee48f7
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-01-30 15:29:43 +0100

    Add light mode icon

--- src/components/ColorModeToggle.tsx
+++ src/components/ColorModeToggle.tsx
@@ -3,18 +3,24 @@ import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
 import type { FunctionComponent } from 'react';
 
-import { DarkModeIcon } from './icons';
+import { DarkModeIcon, LightModeIcon } from './icons';
 
 export const ColorModeToggle: FunctionComponent = () => {
   const { _ } = useLingui();
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <IconButton
       flexShrink="0"
       aria-label={_(t`Toggle color mode`)}
       variant="outline"
-      icon={<DarkModeIcon width="0.75rem" />}
+      icon={
+        colorMode === 'light' ? (
+          <DarkModeIcon width="0.75rem" />
+        ) : (
+          <LightModeIcon width="0.75rem" />
+        )
+      }
       onClick={toggleColorMode}
     />
   );
